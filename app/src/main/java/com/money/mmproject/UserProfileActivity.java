925d7f2ce06dc8c534edcaaee2ae86f77bd19370
Nessa <Dao_ri_zan@hotmail.com>
2015-11-23 18:19:05 -0800

    Update

--- app/src/main/java/com/money/mmproject/UserProfileActivity.java
+++ app/src/main/java/com/money/mmproject/UserProfileActivity.java
@@ -11,6 +11,10 @@ import android.widget.EditText;
  * Created by Nessa on 11/20/15.
  */
 public class UserProfileActivity extends AppCompatActivity {
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,14 @@ public class UserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
 
     }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.SaveButton:
+                return;
+
+        }
+    }
+
 
 }
