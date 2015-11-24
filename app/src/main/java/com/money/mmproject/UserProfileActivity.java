9bdad672e220d463cbd38a231a092632237587a4
Nessa <Dao_ri_zan@hotmail.com>
2015-11-23 17:47:18 -0800

    User profile
    -Updated User Profile

--- app/src/main/java/com/money/mmproject/UserProfileActivity.java
+++ app/src/main/java/com/money/mmproject/UserProfileActivity.java
@@ -1,7 +1,7 @@
 package com.money.mmproject;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,13 +10,13 @@ import android.widget.EditText;
 /**
  * Created by Nessa on 11/20/15.
  */
-public class UserProfileActivity extends Activity {
+public class UserProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_user_profile);
 
-
     }
+
 }
