5ce66f240a3da15f85d81bafd3aea232aba33059
Nessa <Dao_ri_zan@hotmail.com>
2015-11-26 00:10:23 -0800

    Update

--- app/src/main/java/com/money/mmproject/UserProfileActivity.java
+++ app/src/main/java/com/money/mmproject/UserProfileActivity.java
@@ -1,5 +1,7 @@
 package com.money.mmproject;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,24 +13,79 @@ import android.widget.Toast;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-
-
+    private EditText FirstNameText;
+    private EditText LastNameText;
+    private EditText IncomeText;
+    private EditText SavingText;
+    private EditText SpendingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_user_profile);
         Button btn = (Button) findViewById(R.id.SaveButton);
+        FirstNameText = (EditText) findViewById(R.id.FirstNameEditText);
+        LastNameText = (EditText) findViewById(R.id.LastNameEditText);
+        IncomeText = (EditText) findViewById(R.id.IncomeEditText);
+        SavingText = (EditText) findViewById(R.id.SavingEditText);
+        SpendingText = (EditText) findViewById(R.id.SpendingEditText);
+        UserProfileDBHandler db = new UserProfileDBHandler(getApplication());
+        Cursor CR = db.getInformation(db);
+        boolean data = false;
+        if(CR.moveToFirst()){
+            System.out.println("THIS HAS DATA.");
+            data = true;
+        }else{
+            System.out.println("THIS HAS NO DATA");
+        }
+
+        if (data) {
+            db.delete(db);
+        }
+
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "User Profile Saved",Toast.LENGTH_LONG).show();
+                String first = FirstNameText.getText().toString();
+                String last = LastNameText.getText().toString();
+                double income = Double.parseDouble(IncomeText.getText().toString());
+                double saving = Double.parseDouble(SavingText.getText().toString());
+                double spending = Double.parseDouble(SpendingText.getText().toString());
+
+                if (FirstNameText.getText().toString().trim().length() > 0
+                        && LastNameText.getText().toString().trim().length() > 0
+                        && IncomeText.getText().toString().trim().length() > 0
+                        && SavingText.getText().toString().trim().length() > 0
+                        && SpendingText.getText().toString().trim().length() > 0) {
+
+                    //Database handler
+                    UserProfileDBHandler db = new UserProfileDBHandler(getApplication());
+                    Cursor CR = db.getInformation(db);
+                    if(CR.moveToFirst()){
+                        System.out.println("THIS HAS DATA.");
+                        db.update(db, first, last, income, saving, spending);
+                        System.out.println(CR.getString(0) + " " + CR.getString(1));
+                    }else{
+                        //insert new values into database
+                        System.out.println("THIS HAS NO DATA");
+                        db.insert(first, last, income, saving, spending);
+                    }
+
+                    Toast.makeText(getBaseContext(), "User Profile Saved",Toast.LENGTH_LONG).show();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Please enter all the information.",
+                            Toast.LENGTH_LONG).show();
+                }
+
                 return;
             }
         });
 
     }
+
+
 
 
 
